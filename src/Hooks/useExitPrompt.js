84703dbcbd72ea2bd165dc2d94760737122af210
oscar <oscaroceguera@gmail.com>
2021-10-06 17:06:31 -0600

    Finished poc

--- src/Hooks/useExitPrompt.js
+++ src/Hooks/useExitPrompt.js
@@ -1,22 +1,17 @@
 import { useState, useEffect } from "react";
 
-const initBeforeUnLoad = (showExitPrompt) => {
-  // const handleBeforeUnload = (e) => {
-  //   e.preventDefault();
-  //   const message =
-  //     "Are you sure you want to leave? All provided data will be lost.";
-  //   e.returnValue = message;
-  //   return message;
-  // };
 
+
+const initBeforeUnLoad = (showExitPrompt) => {
   window.onbeforeunload = (event) => {
     if (showExitPrompt) {
       const e = event || window.event;
       e.preventDefault();
       if (e) {
+      
         e.returnValue = "";
-      }
 
+      }
       return "";
     }
   };
@@ -42,17 +37,3 @@ export default function useExitPrompt(bool) {
   return [showExitPrompt, setShowExitPrompt];
 }
 
-// useEffect(() => {
-//   window.addEventListener("beforeunload", handleBeforeUnload);
-//   return () => {
-//     window.removeEventListener("beforeunload", handleBeforeUnload);
-//   };
-// }, []);
-
-// const handleBeforeUnload = (e) => {
-//   e.preventDefault();
-//   const message =
-//     "Are you sure you want to leave? All provided data will be lost.";
-//   e.returnValue = message;
-//   return message;
-// };
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,5 +1,4 @@
-import React from "react";
+import React from 'react'
 
-const Dashboard = () => <h1>Dashboard!</h1>;
-
-export default Dashboard;
+const Dashboard = () => <h1>Dashboard</h1>
+export default Dashboard
